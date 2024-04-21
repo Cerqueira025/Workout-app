0463c17535172ee259f0e595c6db04d80e8da038
a104089 <a104089@alunos.uminho.pt>
2024-04-21 17:00:25 +0100

    Adicionadas algumas subclasses de Atividade e início da classe Utilizador

---
+++ new_workout_app/Pesos.java
@@ -0,0 +1,55 @@
+public abstract class Pesos extends Repeticoes{
+    
+    private double peso;
+
+    
+    // ------------------- Construtores ------------------- //
+
+
+    public Pesos() {
+        super();
+        this.peso = 0;
+    }
+
+    public Pesos(String codigo, String descricao, int duracao, Utilizador user, int repeticoes, double peso) {
+        super(codigo, descricao, duracao, user, repeticoes);
+        this.peso = peso;
+    }
+
+    public Pesos(Pesos outro) {
+        super(outro);
+        this.peso = outro.getPeso();
+    }
+
+
+    // ------------------- Métodos ------------------- //
+
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Pesos{" +
+                super.toString() +
+                "peso=" + this.peso +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Pesos pesos = (Pesos) o;
+        return super.equals(pesos)
+                && Double.compare(this.peso,pesos.getPeso()) == 0;
+    }
+
+    public abstract double calorias();
+    public abstract Pesos clone();
+}
